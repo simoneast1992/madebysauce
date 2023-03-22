7f706329a3680e85d2ac00b50ea3f28e852cf67e
Simon East <simoneast1992@gmail.com>
2023-03-22 20:44:51 +0000

    Setup the project
    Set up React app. Add typescript and scss functionality

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/style.scss';
+
+const App: React.FC = () => {
+	return (
+		<div className="app">
+			Made by Sauce - Currency Converter
+		</div>
+	);
+}
+
+export default App;
